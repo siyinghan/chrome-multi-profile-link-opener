1f3d81018b9f9c568a7ffa7479fe09e640d7c4ad
siying <siyinghan@foxmail.com>
2023-02-28 16:15:38 +0100

    add open_chrome.py

--- main.py
+++ main.py
@@ -1,55 +1,11 @@
 """
-Run scripts to open a link with different Chrome profiles at once.
+Open a specified link in Google Chrome using different profiles in the command line.
 """
-import logging
-import sys
 from pathlib import Path
-from subprocess import call
 from sys import argv
-from time import sleep
 
-logging.basicConfig(
-    level=logging.INFO,
-    stream=sys.stdout,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
-
-def open_chrome(link):
-    """
-    Opens a URL in Google Chrome for multiple profiles.
-
-    Parameters:
-        link (str): The URL to open in Google Chrome.
-
-    Returns:
-        None
-
-    Raises:
-        OSError: If the `open_chrome.sh` script cannot be found.
-
-    Example:
-        >>> link = "https://www.example.com"
-        >>> open_chrome(link)
-        Opens the URL "https://www.example.com" in Google Chrome for multiple profiles.
-
-    """
-    # profiles = ["Profile 2", "Profile 3", "Profile 4", "Profile 5", "Profile 7", "Profile 8", "Profile 9",
-    #             "Profile 10", "Profile 11"]
-    profiles = ["Profile 2", "Profile 3"]
-    path = Path(__file__).resolve().parent / "open_chrome.sh"
-
-    # Check if the script exists before attempting to call it
-    if not path.exists():
-        raise OSError("open_chrome.sh script not found")
-
-    for profile in profiles:
-        call([str(path), profile, link])
-        logging.info(f"Opened ({profile}): {link}")
-        sleep(1)
-
-    logging.info("All profiles opened successfully!")
-
+from open_chrome import open_chrome
 
 if __name__ == "__main__":
-    open_chrome(argv[1])
+    sh_path = Path(__file__).resolve().parent
+    open_chrome(sh_path, argv[1])
---
+++ open_chrome.py
@@ -0,0 +1,62 @@
+"""
+Run scripts to open a link with different Chrome profiles at once.
+"""
+import logging
+import sys
+
+from pathlib import Path
+from subprocess import call
+from time import sleep
+
+logging.basicConfig(
+    level=logging.INFO,
+    stream=sys.stdout,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def open_chrome(sh_path: Path, link: str) -> None:
+    """
+    Opens a URL in Google Chrome with different profiles.
+
+    Parameters:
+        sh_path (Path): Path to the directory containing the `open_chrome.sh` script.
+        link (str): The URL to open in Google Chrome.
+
+    Raises:
+        OSError: If the `open_chrome.sh` script cannot be found.
+
+    Returns:
+        None
+
+    Example:
+        >>> link = "https://www.example.com"
+        >>> sh_path = Path("/path/to/directory")
+        >>> open_chrome(sh_path, link)
+        Opens the URL "https://www.example.com" in Google Chrome with different profiles.
+
+    """
+    # Profiles to open
+    # profiles = ["Profile 2", "Profile 3", "Profile 4", "Profile 5", "Profile 7", "Profile 8", "Profile 9",
+    #             "Profile 10", "Profile 11"]
+    profiles = ["Profile 2", "Profile 3"]
+
+    # Path to the `open_chrome.sh` script
+    path = sh_path / "open_chrome.sh"
+
+    # Check if the script exists before attempting to call it
+    if not path.exists():
+        raise OSError("open_chrome.sh script not found")
+
+    # Set permissions on `open_chrome.sh` if necessary
+    if not path.stat().st_mode & 0o100:
+        path.chmod(0o755)
+
+    try:
+        for profile in profiles:
+            call([str(path), profile, link])
+            logging.info(f"Opened ({profile}): {link}")
+            sleep(1)
+        logging.info("All profiles opened successfully!")
+    except OSError as e:
+        logging.error(f"Error opening link: {e}")
